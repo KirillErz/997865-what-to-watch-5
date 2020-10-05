8d032e3dac5f4322ab224d26adde0c1d0bce799c
KirillErz <erzylev@gmail.com>
2020-10-05 20:25:44 +0300

    5. Личный проект: начинаем программировать 2

---
+++ src/components/app/app.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+import StartScreen from "../start-screen/start-screen";
+
+
+
+const App = (props) => {
+  const {properties} = props;
+  const genre = properties.genre;
+  const releaseDate = properties.releaseDate;
+  //console.log(genre);
+  return (
+    <StartScreen
+      genre = {genre}
+      releaseDate = {releaseDate}
+    />
+  );
+};
+// как сделать так что бы проверить сразу свойства объекта ?
+App.propTypes = {
+  properties: PropTypes.shape({
+    genre: PropTypes.string,
+    releaseDate: PropTypes.number
+  })
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app";
+
+const Properties = {
+  genre: "comedy",
+  releaseDate: 1922
+}
+
+ReactDOM.render(
+    <App
+      properties = {Properties}
+    />,
+    document.querySelector(`#root`)
+);
